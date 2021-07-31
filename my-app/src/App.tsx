50892e657adf804d7541d1e41682a45152d6f801
Alejandro Baez <nativo.alejandro@gmail.com>
2021-07-31 14:47:52 -0500

    added filter search

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Coin } from './features/coin/Coin';
+import { CoinPage } from './features/coin/Coin';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Coin />
+      <CoinPage />
     </div>
   );
 }
--- my-app/src/features/coin/Coin.tsx
+++ my-app/src/features/coin/Coin.tsx
@@ -5,27 +5,59 @@ import { Table } from '../../components/table/Table'
 import { CoinDetail } from '../../components/coin_detail/CoinDetail'
 import { coinSelector } from './coinSlice'
 
-export function Coin() {
-	const [coin, setCoin] = React.useState(null)
+export type Coin = {
+	id: string,
+	symbol: string,
+	name: string,
+	nameid: string,
+	rank: number,
+	price_usd: string,
+	percent_change_24h: string,
+	price_btc: string,
+	market_cap_usd: string,
+}
+
+export function CoinPage() {
 	const coins = useAppSelector(coinSelector)
 	const dispatch = useAppDispatch()
+	const [coin, setCoin] = React.useState(null)
+	const [foundCoins, setFoundCoins] = React.useState(coins)
+	const [filterValue, setFilterValue] = React.useState('')
 
 	React.useEffect(() => {
 		dispatch({ type: sagaActions.FETCH_COINS_SAGA })
 	}, [dispatch])
+
+	React.useEffect(() => {
+		if (!coins?.length) return
+		setFoundCoins(coins)
+	}, [coins])
 
 	const handleOnSelectRow = (coinId: string) => {
 		const [coinFound] = coins.filter(({ id }) => id === coinId)
 		setCoin(coinFound)
 	}
 
-	const Filters = () => null
+	const handleOnChangeFilterValue = (event: any) => {
+		const { value } = event.target
+		if (value !== '') {
+			const results: Coin[] | any = coins?.filter((coin: Coin) => coin.name.toLowerCase().startsWith(value.toLowerCase()))
+			setFoundCoins(results)
+		} else {
+			setFoundCoins(coins)
+		}
+		setFilterValue(value)
+	}
+
+	const Filters = ({ children }: { children: any }) => <div>{children}</div>
 	const Pagination = () => null
 
 	return (
 		<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-			<Filters />
-			<Table data={coins} headers={['SYMBOL', 'NAME', '$USD']} onSelectRow={handleOnSelectRow} />
+			<label>
+				<input type="text" value={filterValue} onChange={handleOnChangeFilterValue} />	
+			</label>
+			<Table data={foundCoins} headers={['SYMBOL', 'NAME', '$USD']} onSelectRow={handleOnSelectRow} />
 			<CoinDetail coin={coin}/>
 			<Pagination />
 		</div>
