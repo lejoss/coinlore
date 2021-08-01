aaf617061a064343a9e645ff15503de1bfe9c33a
Alejandro Baez <nativo.alejandro@gmail.com>
2021-08-01 17:50:55 -0500

    added layout syles

--- my-app/src/components/coin_detail/CoinDetail.tsx
+++ my-app/src/components/coin_detail/CoinDetail.tsx
@@ -1,4 +1,3 @@
-import styles from './CoinDetail.module.css'
 import { Table } from '../table/Table'
 export type CoinDetailsProps = {
 	coin?: {
@@ -13,32 +12,33 @@ export type CoinDetailsProps = {
 
 }
 export function CoinDetail(props: CoinDetailsProps) {
-	console.log([props.coin])
 	return (
-		<Table
-			className={props.className}
-			data={[props.coin]}
-			headers={['RANK', 'SYMBOL', 'NAME', 'PRICE $USD', 'PECENT CHANGE 24H', 'MARKET CAP USD']}
-		>
-			{(rows: []) => {
-				return (
-					<>
-						{rows?.map(({ id, rank, symbol, name, price_usd, percent_change_24h, market_cap_usd }) => {
-							return (
-								<tr style={{ color: 'gray' }} key={id}>
-									<td><strong>{rank}</strong></td>
-									<td><strong>{symbol}</strong></td>
-									<td><strong>{name}</strong></td>
-									<td><strong>${price_usd}</strong></td>
-									<td><strong>${percent_change_24h}</strong></td>
-									<td><strong>${market_cap_usd}</strong></td>
-								</tr>
-							)
-						})}
-					</>
-				)
+		<div style={{ overflow: 'scroll' }}>
+			<Table
+				className={props.className}
+				data={[props.coin]}
+				headers={['RANK', 'SYMBOL', 'NAME', 'PRICE $USD', 'PECENT CHANGE 24H', 'MARKET CAP USD']}
+			>
+				{(rows: []) => {
+					return (
+						<>
+							{rows?.map(({ id, rank, symbol, name, price_usd, percent_change_24h, market_cap_usd }) => {
+								return (
+									<tr style={{ color: 'gray' }} key={id}>
+										<td><strong>{rank}</strong></td>
+										<td><strong>{symbol}</strong></td>
+										<td><strong>{name}</strong></td>
+										<td><strong>${price_usd}</strong></td>
+										<td><strong>${percent_change_24h}</strong></td>
+										<td><strong>${market_cap_usd}</strong></td>
+									</tr>
+								)
+							})}
+						</>
+					)
 
-			}}
-		</Table>
+				}}
+			</Table>
+		</div>
 	)
 }
--- my-app/src/features/pagination/pagination.tsx
+++ my-app/src/features/pagination/pagination.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import styles from './Pagination.module.css'
 
 export const Pagination = ({
 	pages,
